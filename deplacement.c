426ac9d060c65426ac0533418f89b5ed76c6cc88
Pual0 <95685052+Pual0@users.noreply.github.com>
2022-04-28 17:40:56 +0200

    deplacement validé

--- deplacement.c
+++ deplacement.c
@@ -52,84 +52,87 @@ void deplacement(BITMAP* terrain, BITMAP* buffer)
         //affichage_terrain(terrain,buffer);
 
         //int matrice_souris[LIGNE][COLONNE];
-        //textprintf_ex(buffer,font,300,300,makecol(255,0,0),1,"SOURIS : %d %d",o,z);
-        //textprintf_ex(buffer,font,400,300,makecol(255,0,0),-1,"%d %d",mouse_x,mouse_y);
-        //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
+
 
         //matrice_souris[i][j];
 
         //clear_bitmap(buffer);
         //affichage_terrain(terrain,buffer);
+        //clear_bitmap(buffer);
+        //affichage_terrain(terrain,buffer);
 
         for (var=0; var<nombre_joueurs; var++)
         {
-        textprintf_ex(buffer,font,500,300,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
-        textprintf_ex(buffer,font,500,400,makecol(255,0,0),-1,": %d %d",matrice_terrain[5][5].x , matrice_terrain[5][5].y);
+        //textprintf_ex(buffer,font,500,300,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
+        //textprintf_ex(buffer,font,500,400,makecol(255,0,0),-1,": %d %d",matrice_terrain[5][5].x , matrice_terrain[5][5].y);
+
         blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);        ///variable de placement initiale a mettre
 
         o = mouse_x/*/34*/;                                                                                                                             ///taille cases
         z = mouse_y/*/34*/;
 
-        textprintf_ex(buffer,font,500,500,makecol(255,0,0),-1,"mouse : %d %d",o,z);
+
+        //textprintf_ex(buffer,font,300,300,makecol(255,0,0),1,"SOURIS : %d %d",o,z);
+        textprintf_ex(buffer,font,300,400,makecol(255,0,0),-1,"%d %d",mouse_x,mouse_y);
+        textprintf_ex(buffer,font,300,500,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
+        //textprintf_ex(buffer,font,500,500,makecol(255,0,0),-1,"mouse : %d %d",o,z);
 
         if(mouse_b&1 && o<acteur[var].x+5*34 && o>acteur[var].x-5*34 && z<acteur[var].x+5*34 && z>acteur[var].y-5*34)                                                                                             ///mettre condition du deplacement
         {
-            if(acteur[var].x<o /*&& acteur[var].y==z*/)
+            if(acteur[var].x<o && z<=acteur[var].y+34 && z>=acteur[var].y)
             {
                 while(acteur[var].x<o)
                 {
                     acteur[var].x=acteur[var].x+34;
                     //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
+                    //textprintf_ex(buffer,font,600,400,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
+                    //clear_bitmap(buffer);
+                    //affichage_terrain(terrain,buffer);
+                    //blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);
+                    //textprintf_ex(buffer,font,600,500,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
+                    //rest(50);
+                }
+            }
+            if(acteur[var].y<z && o<=acteur[var].x+34 && o>=acteur[var].x)
+            {
+                while(acteur[var].y<z)
+                {
+                    acteur[var].y=acteur[var].y+34;
+                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
                     //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
-                    clear_bitmap(buffer);
-                    affichage_terrain(terrain,buffer);
-                    blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);
-                    textprintf_ex(buffer,font,500,300,makecol(255,0,0),-1,"PION : %d %d",acteur[var].x , acteur[var].y);
+                    //clear_bitmap(buffer);
+                    //affichage_terrain(terrain,buffer);
+                    //blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);
+                    //rest(50);
+                }
+            }
+            if(acteur[var].x>o && z<=acteur[var].y+34 && z>=acteur[var].y)
+            {
+                while(acteur[var].x>o)
+                {
+                    acteur[var].x=acteur[var].x-34;
+                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
+                    //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
+                    //clear_bitmap(buffer);
+                    //affichage_terrain(terrain,buffer);
+                    //blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);
+                    //rest(50);
+                }
+            }
+            if(acteur[var].y>z && o<=acteur[var].x+34 && o>=acteur[var].x)
+            {
+                while(acteur[var].y>z)
+                {
+                    acteur[var].y=acteur[var].y-34;
+                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
+                    //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
+                    //clear_bitmap(buffer);
+                    //affichage_terrain(terrain,buffer);
+                    //blit(image_joueur[var], buffer, 0 ,0, acteur[var].x, acteur[var].y, image_joueur[var]->w, image_joueur[var]->h);
                     //rest(50);
                 }
             }
 
-
-
-            /*if(a==o && b<z)
-            {
-                while(b<z)
-                {
-                    b++;
-                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
-                    //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
-                    clear_bitmap(buffer);
-                    affichage_terrain(terrain,buffer);
-                    blit(image_joueur[var], buffer, 0 ,0, matrice_terrain[b][a].x, matrice_terrain[b][a].y, image_joueur[var]->w, image_joueur[var]->h);
-                    //rest(50);
-                }
-            }
-            if(a>o && b==z)
-            {
-                while(a>o)
-                {
-                    a--;
-                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
-                    //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
-                    clear_bitmap(buffer);
-                    affichage_terrain(terrain,buffer);
-                    blit(image_joueur[var], buffer, 0 ,0, matrice_terrain[b][a].x, matrice_terrain[b][a].y, image_joueur[var]->w, image_joueur[var]->h);
-                    //rest(50);
-                }
-            }
-            if(a==o && b>z)
-            {
-                while(b>z)
-                {
-                    b--;
-                    //textprintf_ex(buffer,font,600,300,makecol(255,0,0),1,"SOURIS 2 : %d %d",o,z);
-                    //textprintf_ex(buffer,font,500,300,makecol(255,0,0),1,"PION : %d %d",a,b);
-                    clear_bitmap(buffer);
-                    affichage_terrain(terrain,buffer);
-                    blit(image_joueur[var], buffer, 0 ,0, matrice_terrain[b][a].x, matrice_terrain[b][a].y, image_joueur[var]->w, image_joueur[var]->h);
-                    //rest(50);
-                }
-            }*/
         }
 
     }
