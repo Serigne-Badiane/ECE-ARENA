e5b2f614760d8debfa88c062073dca2aa9d5bbaf
Juliendouvry <95685205+Juliendouvry@users.noreply.github.com>
2022-05-10 19:17:19 +0200

    map final

--- main.c
+++ main.c
@@ -10,11 +10,7 @@ void initialisation()
 {
     allegro_init();
     set_color_depth(desktop_color_depth());
-<<<<<<< HEAD
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,1280,720,0,0)!=0)
-=======
-    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
->>>>>>> cbb83eac42415189733a97caabc38e0fb35cd62e
     {
         allegro_message("prb gfx mode");
         allegro_exit();
@@ -32,13 +28,8 @@ int main()
     srand(time(NULL));
     initialisation();
     //menu();
-<<<<<<< HEAD
     BITMAP* ciel = load_bitmap("ciel.bmp", NULL);
     BITMAP* terrain= load_bitmap("vrai_map.bmp", NULL);
-=======
-
-    BITMAP* terrain= load_bitmap("terrain_normal_grand.bmp", NULL);
->>>>>>> cbb83eac42415189733a97caabc38e0fb35cd62e
     BITMAP* buffer = create_bitmap(SCREEN_W, SCREEN_H);
 
     init_struct_case();
