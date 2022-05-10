545c299940402a3f382b31069ba47f16c9c151eb
ViviLaVesqui <victor.chevallier@edu.ece.fr>
2022-05-10 12:37:09 +0200

    Menu2

--- menu.c
+++ menu.c
@@ -15,14 +15,15 @@ int menu()
     BITMAP* NewGame;
     BITMAP* Credits;
     BITMAP* Regles;
+    BITMAP* Menu2;
 
     SAMPLE *son;
 
 
 
-    allegro_init();-
+    /*allegro_init();
     install_keyboard();
-    install_mouse();
+    install_mouse();*/
 
 
     son = load_wav("clash-of-clans-raiding-musicvolume-increased.wav");
@@ -40,14 +41,14 @@ int menu()
    }
 
 
-    set_color_depth(desktop_color_depth());
+    /*set_color_depth(desktop_color_depth());
 
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,1400,750,0,0)!=0)
     {
         allegro_message("prb gfx mode");
         allegro_exit();
         exit(EXIT_FAILURE);
-    }
+    }*/
 
     play_sample(son, 255, 128, 1000, 1);
 
@@ -64,7 +65,7 @@ int menu()
     clear_bitmap(page);
     if (!decor)
     {
-        allegro_message("pas pu trouver images/hotplanet.bmp");
+        allegro_message("pas pu trouver ");
         exit(EXIT_FAILURE);
     }
 
@@ -87,7 +88,7 @@ int menu()
         blit(decor,page,0,0,0,0,SCREEN_W,SCREEN_H);
 
 
-        if (mouse_y > 300 && mouse_y < 400 && mouse_x > 500 && mouse_x < 850)
+        if (mouse_y > 300 && mouse_y < 400 && mouse_x > 500 && mouse_x < 850) /// Petite animation quand on passe la souris
         {
 
 
@@ -110,6 +111,40 @@ int menu()
             rect(screen,500,600,850,700,makecol(0,100,255));
 
         }
+
+        if (mouse_y > 300 && mouse_y < 400 && mouse_x > 500 && mouse_x < 850 && mouse_b & 1) /// Click sur New Game
+        {
+
+
+            show_mouse(Menu2);
+
+
+            blit(decor,Menu2,0,0,0,0,SCREEN_W,SCREEN_H);
+
+            blit(Menu2,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+
+            rect(screen,500,300,850,400,makecol(0,255,255));
+            rect(screen,500,450,850,550,makecol(255,0,255));
+            rect(screen,500,600,850,700,makecol(0,100,255));
+
+
+        }
+
+        /*if (mouse_y > 450 && mouse_y < 550 && mouse_x > 500 && mouse_x < 850 && mouse_b & 1) /// Click sur Credits
+        {
+
+
+
+
+        }
+
+        if (mouse_y > 600 && mouse_y < 700 && mouse_x > 500 && mouse_x < 850 && mouse_b & 1) /// Click sur Regles
+        {
+
+
+
+
+        }*/
 
 }
 }
