944bd9dfd0174deed90b8920e736cdc04c6395a2
Juliendouvry <95685205+Juliendouvry@users.noreply.github.com>
2022-05-11 22:05:25 +0200

    menu credit condition

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main()
 {
     srand(time(NULL));
     initialisation();
-    //menu();
+    menu();
     BITMAP* terrain= load_bitmap("vrai_map.bmp", NULL);
     BITMAP* buffer = create_bitmap(SCREEN_W, SCREEN_H);
 
@@ -44,7 +44,6 @@ int main()
                 textprintf_ex(buffer,font,matrice_terrain[i][j].x-13,matrice_terrain[i][j].y-7,makecol(0,0,0),-1,"%d %d",i,j);
             }
         }
-
         quadrillage(buffer,terrain);
         draw_sprite(screen, buffer, 0,0);
     }
